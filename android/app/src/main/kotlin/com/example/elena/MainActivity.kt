cbc13de8249dcc3fd093119760cb0947d8d6963f
Muhammad Ghilman Firdaus <ghilman6@gmail.com>
2022-10-04 11:35:24 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/elena/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.elena
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/config/app_information.dart
@@ -0,0 +1,9 @@
+class AppInformation {
+  static const name = 'elena';
+  static const title = 'Elena';
+  static const appIdAndroid = "";
+  static const appIdIos = "";
+  static const masterAdminEmail = "";
+  static const googleMapsKey = '';
+  static const googleMapsKeyURL = '';
+}
